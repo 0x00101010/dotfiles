7a949a2ebfbbae7d415f2daac4c21be120bc08fd
Francis Li <lihuiyi0406@gmail.com>
2025-09-02 11:18:44 -0700

    Initial setup

---
+++ setup-mac.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Install package manager homebrew
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+# # install pre-requisites
+brew update
+brew install chezmoi
+
+# configure CHEZMOI repo
+git clone git@github.com:0x00101010/dotfiles.git
+rm -rf ~/.local/share/chezmoi
+ln -s ~/src/dotfiles ~/.local/share/chezmoi
+chezmoi init
