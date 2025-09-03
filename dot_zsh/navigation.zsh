9b051216fbe48f0d5ff04b224aa8bed470f91df7
Francis Li <lihuiyi0406@gmail.com>
2025-09-02 20:00:58 -0700

    Added bunch of configurations

---
+++ dot_zsh/navigation.zsh
@@ -0,0 +1,5 @@
+# navigation
+cx() { cd "$@" && l; }
+fcd() { cd "$(find . -type d -not -path '*/.*' | fzf)" && l; }
+f() { echo "$(find . -type f -not -path '*/.*' | fzf)" | pbcopy }
+fv() { nvim "$(find . -type f -not -path '*/.*' | fzf)" }
---
+++ dot_zsh/zsh-autocomplete.zsh
@@ -0,0 +1,7 @@
+source $(brew --prefix)/share/zsh-autosuggestions/zsh-autosuggestions.zsh
+bindkey '^w' autosuggest-execute
+bindkey '^e' autosuggest-accept
+bindkey '^u' autosuggest-toggle
+bindkey '^L' vi-forward-word
+bindkey '^k' up-line-or-search
+bindkey '^j' down-line-or-search
