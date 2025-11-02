972707056cd18750556a33f604dd2a231c71a6ac
Francis Li <francis.li@uniswap.org>
2025-11-02 10:37:56 -0800

    Add x permission to setup scripts

--- setup-linux.sh
+++ setup-linux.sh
@@ -8,9 +8,10 @@ sudo apt upgrade -y
 sudo sh -c "$(curl -fsLS get.chezmoi.io)" -- -b /usr/local/bin
 
 # configure CHEZMOI repo
-git clone https://github.com/0x00101010/dotfiles.git
+git clone https://github.com/0x00101010/dotfiles.git ~/src/dotfiles
 rm -rf ~/.local/share/chezmoi
 mkdir -p ~/.local/share
 ln -s ~/src/dotfiles ~/.local/share/chezmoi
+
 chezmoi init
 chezmoi apply -v
