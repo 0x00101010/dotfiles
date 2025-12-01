fc32b59f13854020b41af1e4e566ae5c8b204f75
Francis Li <francis.li@uniswap.org>
2025-12-01 09:25:38 -0800

    Added some public env vars that is used frequently

---
+++ dot_zsh/env.zsh
@@ -0,0 +1,9 @@
+export UNI_SEPOLIA_URL=https://sepolia.unichain.org
+export UNI_MAINNET_URL=https://mainnet.unichain.org
+export UNI_SEPOLIA_FB_WSS=wss://sepolia-flashblocks.unichain.org/ws
+export UNI_MAINNET_FB_WSS=wss://mainnet-flashblocks.unichain.org/ws
+
+export BASE_SEPOLIA_URL=https://sepolia.base.org
+export BASE_MAINNET_URL=https://mainnet.base.org
+export BASE_SEPOLIA_FB_WSS=wss://sepolia.flashblocks.base.org/ws
+export BASE_MAINNET_FB_WSS=wss://mainnet.flashblocks.base.org/ws
