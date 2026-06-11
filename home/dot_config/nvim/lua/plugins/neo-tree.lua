73ced950b7c0949e451a75e9527ad7e8259e8d83
Francis Li <francis.li@coinbase.com>
2026-06-11 23:41:51 +0000

    show hidden file for lazyvim

---
+++ home/dot_config/nvim/lua/plugins/neo-tree.lua
@@ -0,0 +1,18 @@
+return {
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    enabled = false,
+  },
+  {
+    "folke/snacks.nvim",
+    opts = {
+      picker = {
+        sources = {
+          explorer = {
+            hidden = true,
+          },
+        },
+      },
+    },
+  },
+}
